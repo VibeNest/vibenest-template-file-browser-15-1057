c7cddad0d92bddd806314d5ae5f7898859680d18
NikitaBabenko <nikichpt@yandex.ru>
2026-07-10 10:46:59 +0300

    Keep File Browser admin secret synchronized

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,13 @@ set -- \
 if [ ! -f /database/filebrowser.db ]; then
   password_hash="$(/filebrowser hash "${ADMIN_PASSWORD}")"
   set -- "$@" --username admin --password "${password_hash}"
+else
+  /filebrowser \
+    --database /database/filebrowser.db \
+    --config /config/settings.json \
+    users update admin \
+    --password "${ADMIN_PASSWORD}" \
+    --perm.admin >/dev/null
 fi
 
 exec /filebrowser "$@"
